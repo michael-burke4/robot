932b8b5b6ef386a4ce5efbd853a95684e9c70cff
Michael Burke <michael_burke4@student.uml.edu>
2023-03-12 15:59:16 -0400

    begin mergining together searching and centering states

--- lab3/main.py
+++ lab3/main.py
@@ -14,18 +14,21 @@ from cozmo.objects import CustomObject, CustomObjectMarkers, CustomObjectTypes
 from statemachine import State, StateMachine
 import numpy as np
 
+def transform(rob_glob_pose, box_glob_pose):
+    rob_global_angle = rob_glob_pose.rotation.angle_z.radians
+    cos_t = np.cos(rob_global_angle)
+    sin_t = np.sin(rob_global_angle)
+    x = rob_glob_pose.position.x
+    y = rob_glob_pose.position.y
+    T_world_to_robot = np.matrix( [[cos_t,  sin_t, -(cos_t * x + sin_t * y)],
+                                   [-sin_t, cos_t, -(-sin_t * x + cos_t * y)],
+                                   [    0,      0, 1]])
+    box_glob_p = np.matrix([[box_glob_pose.position.x],
+                            [box_glob_pose.position.y],
+                            [1]])
+    return np.matmul(T_world_to_robot, box_glob_p)
+
 # returns 0 if centered, -1 if to the left, 1 if to the right.
-def check_center(box, robot):
-    b_pos = box.pose.position
-    b_angle = np.arctan(b_pos.y / b_pos.x)
-    rob_angle = robot.pose.rotation.angle_z.radians
-    diff = rob_angle - b_angle
-    if np.abs(diff) < .1:
-        return 0
-    elif diff > 0:
-        return 1
-    else:
-        return -1
 
 class CozmoMachine(StateMachine):
     searching = State('Searching', initial=True)
@@ -38,33 +41,51 @@ class CozmoMachine(StateMachine):
     begin_approach = centering.to(approaching)
     wait = approaching.to(waiting)
     lose_center = approaching.to(centering)
-    # lose_track = approaching.to(searching)
-    # wait = approaching.to(waiting)
-    # search_again = waiting.to(searching)
 
     def on_enter_searching(self):
         print("searching!")
-        self.robot.say_text("Searching").wait_for_completed()
-        self.robot.drive_wheels(l_wheel_speed=10, r_wheel_speed=-10)
-        found = False
-        while not found:
-        #     print(self.robot.pose)
-            time.sleep(.1)
+        #self.robot.say_text("Searching").wait_for_completed()
+        # found = False
+        wheelspeed = 10
+        turning_cw = True
+        centered = False
+        self.robot.drive_wheels(wheelspeed, -wheelspeed)
+        while not centered:
+            time.sleep(.05)
             objs = self.robot.world.visible_objects
             try:
                 for obj in objs:
                     if obj.object_type == CustomObjectTypes.CustomType01:
-                        self.robot.stop_all_motors()
-                        found = True
+                        box_rel_pose = transform(self.robot.pose, obj.pose)
+                        rel_angle = np.arctan(box_rel_pose[1] / box_rel_pose[0])
+                        # print(rel_angle)
+                        if (rel_angle < -.1):
+                            if not turning_cw:
+                                self.robot.stop_all_motors()
+                                self.robot.drive_wheels(wheelspeed, -wheelspeed)
+                                turning_cw = True
+                        elif (rel_angle > .1):
+                            if turning_cw:
+                                self.robot.stop_all_motors()
+                                self.robot.drive_wheels(-wheelspeed, wheelspeed)
+                                turning_cw = False
+                        else:
+                            self.robot.stop_all_motors()
+                            time.sleep(5)
+                            print("done sleepin!")
+                            return
+                            
             except:
+                print("SHOULDN'T GET HERE OFTEN!")
                 pass
-        self.center()
+        # self.center()
 
     def on_enter_centering(self):
         print("Centering!")
-        self.robot.say_text("Centering").wait_for_completed()
+        #self.robot.say_text("Centering").wait_for_completed()
         centered = False
         still_see = True
+        self.robot.drive_wheels(10, -10)
         while (not centered) and still_see:
             time.sleep(.1)
             objs = self.robot.world.visible_objects
@@ -76,10 +97,14 @@ class CozmoMachine(StateMachine):
                         center_status = check_center(obj, self.robot)
                         if center_status == 0:
                             centered = True
+                            print("GOT HERE!")
                             self.robot.stop_all_motors()
-                        else:
-                            self.robot.drive_wheels(10 * center_status, -10 * center_status)
+                            while True:
+                                time.sleep(.1)
+                                pass
+                        self.robot.drive_wheels(center_status * 10, center_status * -10)
             except:
+                print("SHOULDNT REALLY  GET HERE")
                 pass
         if not still_see:
             self.robot.stop_all_motors()
@@ -115,11 +140,11 @@ def run(robot: cozmo.robot.Robot):
     circle_cube = robot.world.define_custom_cube(CustomObjectTypes.CustomType00,
                                               CustomObjectMarkers.Circles2,
                                               44,
-                                              30, 30, True)
+                                              31, 31, True)
     diamond_cube = robot.world.define_custom_cube(CustomObjectTypes.CustomType01,
                                               CustomObjectMarkers.Diamonds2,
                                               44,
-                                              30, 30, True)
+                                              31, 31, True)
     
     machine = CozmoMachine(robot)
 
